29351b641f20da6b29e9c3df334c1d575b7a232a
Arunava Dev <arunavadasgupta1996@gmail.com>
2026-08-14 23:29:06 +0530

    feat: implement GetAllContainer

--- AzureBlobProject/Services/ContainerService.cs
+++ AzureBlobProject/Services/ContainerService.cs
@@ -1,4 +1,5 @@
 ﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,9 +27,14 @@ namespace AzureBlobProject.Services
             throw new NotImplementedException();
         }
 
-        public Task<List<string>> GetAllContainer()
+        public async Task<List<string>> GetAllContainer()
         {
-            throw new NotImplementedException();
+            List<string> containerName = new();
+            await foreach(BlobContainerItem blobContainerItem in _blobClient.GetBlobContainersAsync())
+            {
+                containerName.Add(blobContainerItem.Name);
+            }
+            return containerName;
         }
 
         public Task<List<string>> GetAllContainerAndBlobs()
