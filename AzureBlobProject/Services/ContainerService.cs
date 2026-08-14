b40892b061f7b8a6803e9d7ded7de376b144e4aa
Arunava Dev <arunavadasgupta1996@gmail.com>
2026-08-14 23:35:04 +0530

    feat: implement CreateContainer

--- AzureBlobProject/Services/ContainerService.cs
+++ AzureBlobProject/Services/ContainerService.cs
@@ -17,9 +17,10 @@ namespace AzureBlobProject.Services
             _blobClient = blobClient;
         }
 
-        public Task CreateContainer(string containerName)
+        public async Task CreateContainer(string containerName)
         {
-            throw new NotImplementedException();
+            BlobContainerClient blobContainerClient = _blobClient.GetBlobContainerClient(containerName);
+            await blobContainerClient.CreateIfNotExistsAsync(PublicAccessType.BlobContainer);
         }
 
         public Task DeleteContainer(string containerName)
