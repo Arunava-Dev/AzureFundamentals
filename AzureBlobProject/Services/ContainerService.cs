b44a23f414cd84b2bc64c0b018caf1dfe3937b81
Arunava Dev <arunavadasgupta1996@gmail.com>
2026-08-14 23:06:18 +0530

    feat: add IContainerService interface and ContainerService implementation

---
+++ AzureBlobProject/Services/ContainerService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureBlobProject.Services
+{
+    public class ContainerService : IContainerService
+    {
+        public Task CreateContainer(string containerName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteContainer(string containerName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<string>> GetAllContainer()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<string>> GetAllContainerAndBlobs()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
