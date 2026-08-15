1d4d6ba6e63aa7b43a6fa163e5eee68a3a550635
Arunava Dev <arunavadasgupta1996@gmail.com>
2026-08-16 00:57:32 +0530

    feat: inject IContainerService into ContainerController

--- AzureBlobProject/Controllers/ContainerController.cs
+++ AzureBlobProject/Controllers/ContainerController.cs
@@ -1,9 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AzureBlobProject.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AzureBlobProject.Controllers
 {
     public class ContainerController : Controller
     {
+        private readonly IContainerService _containerService;
+
+        public ContainerController(IContainerService containerService)
+        {
+            _containerService = containerService;
+        }
+
         public IActionResult Index()
         {
             return View();
