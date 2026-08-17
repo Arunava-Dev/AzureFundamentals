ad440e6591a6838c7e2222d26bbe3cf6137ab420
Arunava Dev <arunavadasgupta1996@gmail.com>
2026-08-17 23:36:04 +0530

    feat : add IBlobService interface and BlobService implementation

---
+++ AzureBlobProject/Services/IBlobService.cs
@@ -0,0 +1,18 @@
+﻿using AzureBlobProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureBlobProject.Services
+{
+    public interface IBlobService
+    {
+        Task<List<string>> GetAllBlobs(string containerName);
+        Task<List<BlobModel>> GetAllBlobsWithUri(string containerName);
+        Task<string> GetBlob(string name,string containerName);
+        Task CreateBlob(string name,IFormFile file, string containerName,BlobModel model);
+        Task DeleteBlob(string name,string containerName);
+    }
+}
