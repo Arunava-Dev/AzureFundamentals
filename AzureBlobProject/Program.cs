a0f45c4f692e8c48f3c84ae86091a5e84ca67210
Arunava Dev <arunavadasgupta1996@gmail.com>
2026-08-18 01:26:58 +0530

    chore : register BlobService in DI as singleton

--- AzureBlobProject/Program.cs
+++ AzureBlobProject/Program.cs
@@ -8,6 +8,7 @@ builder.Services.AddControllersWithViews();
 // Adding BlobServiceClient
 builder.Services.AddSingleton(u => new BlobServiceClient(builder.Configuration.GetValue<string>("BlobConnection")));
 builder.Services.AddSingleton<IContainerService,ContainerService>();
+builder.Services.AddSingleton<IBlobService, BlobService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
