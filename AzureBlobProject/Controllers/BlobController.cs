a58f39e5fcac05e5c2c20e027457083f74881da2
Arunava Dev <arunavadasgupta1996@gmail.com>
2026-08-18 12:11:24 +0530

    feat : inject IBlobService into BlobController

--- AzureBlobProject/Controllers/BlobController.cs
+++ AzureBlobProject/Controllers/BlobController.cs
@@ -1,9 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AzureBlobProject.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AzureBlobProject.Controllers
 {
     public class BlobController : Controller
     {
+        private readonly IBlobService _blobService;
+
+        public BlobController(IBlobService blobService)
+        {
+            _blobService = blobService;
+        }
+
         public IActionResult Index()
         {
             return View();
