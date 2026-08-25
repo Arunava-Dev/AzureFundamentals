d79b3027467d55eb0736a0a7a2c31ebd57c6ed8f
Arunava Dev <arunavadasgupta1996@gmail.com>
2026-08-25 11:46:06 +0530

    feat: implement GetAllBlobsWithUri

--- AzureBlobProject/Services/BlobService.cs
+++ AzureBlobProject/Services/BlobService.cs
@@ -72,9 +72,33 @@ namespace AzureBlobProject.Services
             return blobNames;
         }
 
-        public Task<List<BlobModel>> GetAllBlobsWithUri(string containerName)
+        public async Task<List<BlobModel>> GetAllBlobsWithUri(string containerName)
         {
-            throw new NotImplementedException();
+            BlobContainerClient blobContainerClient = _blobClient.GetBlobContainerClient(containerName);
+            var blobs = blobContainerClient.GetBlobsAsync();
+
+            List<BlobModel> blobList = new List<BlobModel>();
+
+            await foreach (var blob in blobs)
+            {
+                var blobClient = blobContainerClient.GetBlobClient(blob.Name);
+
+                BlobModel blobModel = new()
+                {
+                    Uri = blobClient.Uri.AbsoluteUri
+                };
+                BlobProperties properties = await blobClient.GetPropertiesAsync();
+                if (properties.Metadata.ContainsKey("title"))
+                {
+                    blobModel.Title = properties.Metadata["title"];
+                }
+                if (properties.Metadata.ContainsKey("comment"))
+                {
+                    blobModel.Comment = properties.Metadata["comment"];
+                }
+                blobList.Add(blobModel);
+            }
+            return blobList;
         }
 
         public async Task<string> GetBlob(string name, string containerName)
