3157f56efd5f3fb05d47621fbd93bbec54209702
Arunava Dev <arunavadasgupta1996@gmail.com>
2026-08-25 12:02:13 +0530

    feat: add SAS URI generation for individual blob

--- AzureBlobProject/Services/BlobService.cs
+++ AzureBlobProject/Services/BlobService.cs
@@ -1,5 +1,7 @@
 ﻿using Azure.Storage.Blobs;
 using Azure.Storage.Blobs.Models;
+using Azure.Storage.Blobs.Specialized;
+using Azure.Storage.Sas;
 using AzureBlobProject.Models;
 using System.Collections;
 
@@ -87,6 +89,22 @@ namespace AzureBlobProject.Services
                 {
                     Uri = blobClient.Uri.AbsoluteUri
                 };
+
+                if(blobClient.CanGenerateSasUri)
+                {
+                    BlobSasBuilder blobSasBuilder = new()
+                    {
+                        BlobContainerName = blobClient.GetParentBlobContainerClient().Name,
+                        BlobName = blobClient.Name,
+                        Resource = "b",
+                        ExpiresOn = DateTimeOffset.UtcNow.AddHours(1)
+                    };
+                    blobSasBuilder.SetPermissions(BlobSasPermissions.Read);
+
+                    blobModel.Uri = blobClient.GenerateSasUri(blobSasBuilder).AbsoluteUri;
+                    
+                }
+
                 BlobProperties properties = await blobClient.GetPropertiesAsync();
                 if (properties.Metadata.ContainsKey("title"))
                 {
