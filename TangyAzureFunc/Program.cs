735250a9135655ca87eff8d3a5c43e77a6ee656d
Arunava Dev <arunavadasgupta1996@gmail.com>
2026-09-01 12:41:24 +0530

    chore: register ApplicationDbContext in Program.cs

--- TangyAzureFunc/Program.cs
+++ TangyAzureFunc/Program.cs
@@ -2,19 +2,31 @@ using Azure.Monitor.OpenTelemetry.Exporter;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Builder;
 using Microsoft.Azure.Functions.Worker.OpenTelemetry;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using OpenTelemetry;
+using TangyAzureFunc.Data;
 
 var builder = FunctionsApplication.CreateBuilder(args);
 
 builder.ConfigureFunctionsWebApplication();
 
-if (!string.IsNullOrEmpty(Environment.GetEnvironmentVariable("APPLICATIONINSIGHTS_CONNECTION_STRING")))
-{
-    builder.Services.AddOpenTelemetry()
-        .UseFunctionsWorkerDefaults()
-        .UseAzureMonitorExporter();
-}
+//if (!string.IsNullOrEmpty(Environment.GetEnvironmentVariable("APPLICATIONINSIGHTS_CONNECTION_STRING")))
+//{
+//    builder.Services.AddOpenTelemetry()
+//        .UseFunctionsWorkerDefaults()
+//        .UseAzureMonitorExporter();
+//}
+
+string connectionString = Environment.GetEnvironmentVariable("SqlDatabase"); //retrive connection string
+
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                 options.UseSqlServer(connectionString));
+
+
+
+
 
 builder.Build().Run();
